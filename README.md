d524cc2e200acd3811536e3d77466b49f7747642
Tonysk08 <zarmiento227@gmail.com>
2020-06-19 17:55:56 -0500

    modulos y dependencias

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Proyecto-Final-DS9
 Proyecto final para el curso de desarrollo de software 9.
+
+1. primer commit navegacion del sitio, prototipo de alta fidelidad.
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path=require('path');
+const morgan = require('morgan');
+const navRoutes = require(path.join(__dirname,'src/routes/routes'));
+
+//moddulo extra para dar color a la consola inecesario
+const colors = require('colors');
+
+const app = express();
+
+//configuracion del server
+app.set('port',process.env.PORT || 3000);
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'src/views'));
+
+//midelwares
+//fija morgan
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+
+//usando rutas
+app.use('/',navRoutes)
+
+//fijando archivos estaticos publicos
+app.use(express.static(path.join(__dirname,'src/public')));
+
+
+app.listen(app.get('port'),function(){
+    console.log('Server running on port '.blue+app.get('port'));
+})
