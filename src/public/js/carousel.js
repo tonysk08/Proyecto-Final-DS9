86581b21ca92d56aea725a0221aeec69b1f27124
Tonysk08 <zarmiento227@gmail.com>
2020-06-23 17:53:51 -0500

    caruser mejorado listo, + carpeta para components

---
+++ src/public/js/carousel.js
@@ -0,0 +1,8 @@
+console.log("carrusel js set");
+
+(function($){
+    "use strict";
+    $('.next').click(function(){$('.carousel').carousel('next');return false;})
+    $('.prev').click(function(){$('.carousel').carousel('prev');return false;})
+})
+(jQuery);
