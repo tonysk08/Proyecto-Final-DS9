9260e72583300dcfb349255121ef59dbfdebafda
Tonysk08 <zarmiento227@gmail.com>
2020-06-24 21:51:52 -0500

    enlaces carrito, pago, fav.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,6 +11,13 @@ router.get('/passchange',(req,res) => { res.render('CambiarPass')});
 router.get('/cobertura',(req,res) => { res.render('cobertura')});
 router.get('/sugerencias',(req,res) => { res.render('sugerencias')});
 router.get('/supermercados',(req,res) => { res.render('supermercados')});
+router.get('/shoppingcart',(req,res) => { res.render('carrito')});
+router.get('/pago',(req,res) => { res.render('pago')});
+router.get('/paquete',(req,res) => { res.render('paquetes')});
+router.get('/pedido',(req,res) => { res.render('pedido')});
+router.get('/favoritos',(req,res) => { res.render('fav')});
+router.get('/categorias',(req,res) => { res.render('categorias')});
+router.get('/newProduct',(req,res) => { res.render('NewProduct')});
 
 
 module.exports = router;
