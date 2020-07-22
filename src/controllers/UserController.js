db1446bbc6c841bbc83d08f8dda8cc509761ced4
Tonysk08 <zarmiento227@gmail.com>
2020-07-21 19:54:59 -0500

    cambios rendimiento

---
+++ src/controllers/UserController.js
@@ -0,0 +1,8 @@
+const controller = {};
+
+
+controller.getSignUp = (req, res, next) => {
+    res.render("Perfil")
+}
+
+module.exports = controller;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,25 +1,26 @@
 const express = require('express');
-//const navControllers = require('../controllers/')
 const router = express.Router();
-//Carrito de compras
-const carritoCtrl = require('../controllers/carrito_controller');
-const landingCtrl = require('../controllers/landingController');
+
+const controllers = require('../controllers/_index');
+
 const { token } = require('morgan');
+
 //stripe
 const stripe = require('stripe')('sk_test_51H5dA2KDd3ZOeKrKQtrJpYAYSS2X8AgqCBOg8zw85ttAb9Jaq3P2EYfz2K13SuoTlFHJLHVHtBAwJJF5zPSK6mii00xxkfhHuI');
 
 
 //router.get('/',(req,res) => { res.render('LandingPage')});
 router.get('/help',(req,res) => { res.render('Ayuda')});
+
 router.get('/login',(req,res) => { res.render('Login')});
-router.get('/profile',(req,res) => { res.render('Perfil')});
+router.get('/auth/profile',controllers.UserController.getSignUp);
 router.get('/registro',(req,res) => { res.render('Registro')});
+
 router.get('/passchange',(req,res) => { res.render('RecuperarPass')});
 router.get('/cobertura',(req,res) => { res.render('Cobertura')});
 router.get('/sugerencias',(req,res) => { res.render('Sugerencias')});
 router.get('/supermercados',(req,res) => { res.render('Supermercados')});
 router.get('/Todos',(req,res) => { res.render('Todos')});
-//router.get('/shoppingcart',(req,res) => { res.render('Carrito')});
 router.get('/pago',(req,res) => { res.render('Pago')});
 router.get('/paquetes',(req,res) => { res.render('Paquetes')});
 router.get('/pedido',(req,res) => { res.render('Pedido')});
@@ -29,8 +30,8 @@ router.get('/newProduct',(req,res) => { res.render('NewProduct')});
 router.get('/oferta',(req,res) => { res.render('Ofertas')});
 
 
-router.get('/shoppingcart', carritoCtrl.list);
-router.get('/', landingCtrl.list);
+router.get('/shoppingcart', controllers.carrito_controller.list);
+router.get('/', controllers.landingController.list);
 
 
 router.post('/checkout', async (req, res) => {
