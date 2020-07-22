dd2ac8671df1a6d53e8f1e3bd52efe889fc8de04
caldgalv <67244286+caldgalv@users.noreply.github.com>
2020-07-22 01:33:02 -0500

    va

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,13 +1,37 @@
 const controller = {};
 var bcrypt = require('bcryptjs');
-
+var mysql = require('mysql');
 
 controller.getSignUp = (req, res, next) => {
     res.render("registro")
 }
 
 controller.postSignUp = (req, res, next) =>{
-    console.log(req.body);
-    return;
+    var salt = bcrypt.genSaltSync(10); //encriptacion
+    var password = bcrypt.hashSync(req.body.password, salt);
+
+    var user =
+    {
+        nombre : req.body.nombre,
+        apellido : req.body.apellido,
+        cedula : req.body.cedula,
+        email : req.body.email,
+        password : password,
+        telefono : req.body.telefono,
+        fecha : req.body.fecha
+    };
+
+
+    controller.list = (req, res) => {
+        req.getConnection((err, conn) => {
+          conn.query('INSERT INTO users SET ?', user, (err, rows, fields) => {
+           if (err) {
+            res.json(err);
+           }
+           res.render('perfil')
+          });
+        });
+      };
+
 } 
 module.exports = controller;
---
+++ src/db/config.js
@@ -0,0 +1,8 @@
+var config = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'virtualmarketdb'
+};
+
+module.exports = config;
